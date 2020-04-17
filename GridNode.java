8a76652dbdf53957fa563c0f4d1dce0d164b4acf
ak2253 <61290235+ak2253@users.noreply.github.com>
2020-04-17 17:49:45 -0400

    Update GridNode.java

--- GridNode.java
+++ GridNode.java
@@ -1,16 +1,14 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 
 public class GridNode {
-	List<Integer> coord=new ArrayList<Integer>(Arrays.asList(null,null));
+	int x;
+	int y;
 	HashMap<GridNode,Integer> neighbors=new HashMap<GridNode,Integer>();
 	String value;
 	boolean visited=false;
 	public GridNode(int x, int y, String value) {
-		coord.set(0,x);
-		coord.set(1,y);
+		this.x=x;
+		this.y=y;
 		this.value=value;
 	}
 }
