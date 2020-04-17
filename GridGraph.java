da4b8f82ef199abe736431b922d2a4d3a3a18850
ak2253 <61290235+ak2253@users.noreply.github.com>
2020-04-17 17:49:22 -0400

    Update GridGraph.java

--- GridGraph.java
+++ GridGraph.java
@@ -1,55 +1,40 @@
 import java.util.HashSet;
 
 public class GridGraph {
-	HashSet<GridNode> Set=new HashSet<GridNode>();
-	void addGridNode(final int x,final int y, final String nodeVal) { //add node to graph
+	HashSet<GridNode> GridSet=new HashSet<GridNode>();
+	void addGridNode(final int x,final int y, final String nodeVal) {
 		GridNode node=new GridNode(x,y,nodeVal);
-		Set.add(node);
+		GridSet.add(node);
 	}
 	
 	void addUndirectedEdge(final GridNode first, final GridNode second) { //connect node first to node second both way
-		if(first.neighbors.containsKey(second)) { //check if neighbors
-			GridNode node1=null;
-			GridNode node2=null;
-			for(GridNode node:Set) { //check if both nodes exist in graph
-				if(node.value.compareTo(first.value)==0)
-					node1=node;
-				if(node.value.compareTo(second.value)==0)
-					node2=node;
-			}
-			if(node1!=null&&node2!=null) { //connect nodes if both nodes exist
-				node1.neighbors.replace(node2,1);
-				node2.neighbors.replace(node1,1);
-			}
+		if(isNeighbor(first,second)&&GridSet.contains(first)&&GridSet.contains(second)) { //check if neighbors
+			first.neighbors.replace(second,1);
+			second.neighbors.replace(first,1);
 		}
 	}
 	
 	void removeUndirectedEdge(final GridNode first, final GridNode second) { //remove edge from node first to node second both ways
-		if(first.neighbors.containsKey(second)) { //check if neighbors
-			GridNode node1=null;
-			GridNode node2=null;
-			for(GridNode node:Set) { //check if both nodes exist in graph
-				if(node.value.compareTo(first.value)==0)
-					node1=node;
-				if(node.value.compareTo(second.value)==0)
-					node2=node;
-			}
-			if(node1!=null&&node2!=null) { //remove edge if both nodes exist
-				node1.neighbors.replace(node2,0);
-				node2.neighbors.replace(node1,0);
-			}
+		if(isNeighbor(first,second)&&GridSet.contains(first)&&GridSet.contains(second)) { //check if neighbors
+			first.neighbors.replace(second,0);
+			second.neighbors.replace(first,0);
 		}
 	}
 	
-	HashSet<GridNode> getAllNodes() { //return set
-		return Set;
+	HashSet<GridNode> getAllNodes() {
+		return GridSet;
 	}
 	
-	public GridNode findGridNode(int x, int y) { //find gridnode in graph
-		for(GridNode node:Set) {
-			if(node.coord.get(0)==x&&node.coord.get(1)==y)
+	public GridNode findGridNode(int x, int y) {
+		for(GridNode node:GridSet) {
+			if(node.x==x&&node.y==y)
 				return node;
 		}
 		return null;
+	}
+	private boolean isNeighbor(GridNode node1,GridNode node2) {
+		if((Math.abs(node1.x-node2.x)==0&&Math.abs(node1.y-node2.y)==1)||(Math.abs(node1.x-node2.x)==1&&Math.abs(node1.y-node2.y)==0))
+			return true;
+		return false;
 	}
 }
