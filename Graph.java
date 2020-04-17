4266472babcc3daa0d9267210f4a096e602f6adf
ak2253 <61290235+ak2253@users.noreply.github.com>
2020-04-17 17:48:25 -0400

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,49 +1,34 @@
 import java.util.HashSet;
 
 public class Graph {
-	public HashSet<Node> Set=new HashSet<Node>();
-	public void addNode(final String nodeVal) { //add node to Set
+	public HashSet<Node> nodeSet;
+	public Graph() {
+		nodeSet=new HashSet<Node>();
+	}
+	public void addNode(final String nodeVal) {
 		Node node=new Node(nodeVal);
-		Set.add(node);
+		nodeSet.add(node);
 	}
 	
 	public void addUndirectedEdge(final Node first, final Node second) { //create edge from node first to node second both ways
-		if(!Set.isEmpty()) { //check for empty set
-			if(Set.contains(first)&&Set.contains(second)) { //check if both nodes exists
-				for(Node node:Set) {
-					if(node==first) { //node first found
-						if(node.elst.indexOf(second)==-1) //check for repeats
-							node.elst.add(second);
-					}
-					if(node==second) { //node second found
-						if(node.elst.indexOf(first)==-1) //check for repeats
-							node.elst.add(first);
-					}
-				}
-			}
+		if(nodeSet.contains(first)&&nodeSet.contains(second)) { //check if both nodes exists
+			if(!first.nodeSet.contains(second)) //check for repeats
+				first.nodeSet.add(second);
+			if(!second.nodeSet.contains(first))
+				second.nodeSet.add(first);
 		}
 	}
 	
 	public void removeUndirectedEdge(final Node first, final Node second) { //delete edge from node first to node second both ways
-		if(!Set.isEmpty()) { //check for empty set
-			if(Set.contains(first)&&Set.contains(second)) { //check if both nodes exist
-				for(Node node:Set) {
-					if(node==first) { //node first is found
-						int index=node.elst.indexOf(second);
-						if(index!=-1) //check if edge exist
-							node.elst.remove(index);
-					}
-					if(node==second) { //node second is found
-						int index=node.elst.indexOf(first);
-						if(index!=-1) //check if edge exist
-							node.elst.remove(index);
-					}
-				}
-			}
+		if(nodeSet.contains(first)&&nodeSet.contains(second)) { //check if both nodes exist
+			if(first.nodeSet.contains(second))
+				first.nodeSet.remove(second);
+			if(second.nodeSet.contains(first))
+				second.nodeSet.remove(first);
 		}
 	}
 	
-	public HashSet<Node> getAllNodes() { //returns Set
-		return Set;
+	public HashSet<Node> getAllNodes() {
+		return nodeSet;
 	}
 }
